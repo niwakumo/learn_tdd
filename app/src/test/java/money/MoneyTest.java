edf91a10dfd474d40e9b7353557a1282a2ae8787
niwakumo <niwakumo.dev@gmail.com>
2024-10-10 03:40:41 +0900

    feat: timesがDollarのインスタンスを返すように変更、チェックリストの追加

--- app/src/test/java/money/MoneyTest.java
+++ app/src/test/java/money/MoneyTest.java
@@ -7,11 +7,11 @@ public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
 
-        five.times(3);
-        assertEquals(15, five.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
 
