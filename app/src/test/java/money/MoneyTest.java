03a1953131a12962d9716cead9e80657d64ef39d
niwakumo <niwakumo.dev@gmail.com>
2024-10-12 00:41:50 +0900

    feat: Francを実装

--- app/src/test/java/money/MoneyTest.java
+++ app/src/test/java/money/MoneyTest.java
@@ -17,5 +17,14 @@ public class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+    @Test
+    public void testFrancMultipication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+
+    }
+
 }
 
