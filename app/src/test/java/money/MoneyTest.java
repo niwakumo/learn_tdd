3525ad87be304259190ef20e360b90967469fa77
niwakumo <niwakumo.dev@gmail.com>
2024-10-14 23:43:51 +0900

    fix: クラス定義の変更

--- app/src/test/java/money/MoneyTest.java
+++ app/src/test/java/money/MoneyTest.java
@@ -14,11 +14,11 @@ public class MoneyTest {
 
     @Test
     public void testEquality() {
-        assertTrue(Money.dollar(5).equals(new Dollar(5)));
-        assertFalse(Money.dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        assertTrue(Money.franc(5).equals(new Franc(5)));
-        assertFalse(Money.franc(5).equals(new Franc(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
 
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
@@ -29,6 +29,12 @@ public class MoneyTest {
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
 
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 
 }
