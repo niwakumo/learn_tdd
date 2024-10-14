8a966d1ab061495c9f67840a2313db671557ef92
niwakumo <niwakumo.dev@gmail.com>
2024-10-15 02:37:59 +0900

    feat: Expression, Bankの実装

--- app/src/main/java/money/Money.java
+++ app/src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -33,6 +33,10 @@ class Money {
 
     public String toString() {
         return amount + " " + currency;
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 
 }
